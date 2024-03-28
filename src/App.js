a4f2790134db9c09ec1881dbaa041c940da7196f
nikolay bulgakov <nikolay.bulgakov@iqoption.com>
2024-03-28 17:47:56 +0200

    fix sendbox

--- src/App.js
+++ src/App.js
@@ -22,7 +22,6 @@ const App = () => {
 
   const copyToClipboard = (text) => {
     navigator.clipboard.writeText(text).then(() => {
-      console.log('Text successfully copied');
     }).catch(err => {
       console.error('Failed to copy text: ', err);
     });
