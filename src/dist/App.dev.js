635b484a7d828acdb8e7262b373559ef3a4d68fa
Nikolay Bulgakov <brokenbass1990@gmail.com>
2024-08-27 17:13:34 +0300

    Added modal window improvements and placeholder management

---
+++ src/dist/App.dev.js
@@ -0,0 +1,80 @@
+// import React, { useState } from 'react';
+// import { observer } from 'mobx-react-lite';
+// import PdfMaker from './components/PdfMaker/PdfMaker.jsx';
+// import HtmlWindow from './components/HtmlWindow/HtmlWindow.jsx';
+// import ConvertButton from './components/ConvertButton/ConvertButton';
+// import TxtToJson from './components/TxtToJson/TxtToJson.jsx';
+// import FileUploader from './components/FileUploader/FileUploader.jsx';
+// import { appStore } from './stores/AppStore';
+// import './App.css';
+// import AddLocaleModal from './components/AddLocaleModal/AddLocaleModal';
+// const App = observer(() => {
+//   const [buttonColors, setButtonColors] = useState({});
+//   const [isModalOpen, setIsModalOpen] = useState(false);
+//   function onElementClick(text) {
+//     if (text) {
+//       appStore.setHighlightedText(text);
+//     }
+//   }
+//   function handleFolderClick(folderName) {
+//     const setButtonColor = (color) => {
+//       setButtonColors((prev) => ({ ...prev, [folderName]: color }));
+//     };
+//     appStore.handleFolderSelection(folderName, setButtonColor);
+//   }
+//   function openAddLocaleModal() {
+//     setIsModalOpen(true);
+//   }
+//   function closeAddLocaleModal() {
+//     setIsModalOpen(false);
+//   }
+//   function handleSaveLocale(data) {
+//     appStore.addLocaleManually(data);
+//   }
+//   return (
+//     <div className="App">
+//       <div className="top-bar">
+//         <div className="file-uploader-container">
+//           <FileUploader onFilesUploaded={appStore.handleFilesUploaded} />
+//         </div>
+//         <button className={`original-btn ${appStore.isOriginalSelected ? 'selected' : ''}`} onClick={appStore.resetLocale}>Original</button>
+//         {appStore.locales.map((locale) => (
+//           <button key={locale} onClick={() => appStore.handleLocaleSelection(locale)} className={`locale-btn ${appStore.selectedLocale === locale ? 'selected' : ''}`}>
+//             {locale}
+//           </button>
+//         ))}
+//         <button className="original-btn" onClick={openAddLocaleModal}>+</button>
+//       </div>
+//       <div className="folder-bar">
+//         {Object.keys(appStore.foldersData).map((folderName) => (
+//           <div key={folderName} className="folder-entry">
+//             <button
+//               onClick={() => handleFolderClick(folderName)}
+//               className={`folder-btn ${appStore.selectedFolder === folderName ? 'selected' : ''}`}
+//               style={{ backgroundColor: buttonColors[folderName] || '' }}
+//             >
+//               {folderName}
+//             </button>
+//             <button onClick={() => appStore.deleteFolder(folderName)} className="delete-button folder-btn">
+//               ✖
+//             </button>
+//           </div>
+//         ))}
+//       </div>
+//       <div className="content-area">
+//         <PdfMaker html={appStore.html} setHtml={appStore.setOriginalHtml.bind(appStore)} highlightedText={appStore.highlightedText} />
+//         <HtmlWindow htmlContent={appStore.html} onElementClick={onElementClick} />
+//       </div>
+//       <div className="buttons-area">
+//         <ConvertButton html={appStore.html} />
+//         <button className="convert-button txt-to-json-toggle" onClick={appStore.toggleTxtToJson}>
+//           {appStore.showTxtToJson ? 'Close Txt to JSON' : 'Txt to JSON'}
+//         </button>
+//       </div>
+//       {appStore.showTxtToJson && <TxtToJson onClose={appStore.toggleTxtToJson} isVisible={appStore.showTxtToJson} />}
+//       {isModalOpen && <AddLocaleModal onClose={closeAddLocaleModal} onSave={handleSaveLocale} />}
+//     </div>
+//   );
+// });
+// export default App;
+"use strict";
--- src/stores/AppStore.js
+++ src/stores/AppStore.js
@@ -1,3 +1,440 @@
+// // import { makeAutoObservable, runInAction } from "mobx";
+// // import { replacePlaceholders } from '../utils/replacePlaceholders';
+// // import { saveSettings, getSettings, deleteSettings } from '../utils/indexedDB'; 
+
+// // class AppStore {
+// //   originalHtml = '';
+// //   html = '';
+// //   htmlByLocale = {};
+// //   foldersData = {};
+// //   selectedFolder = 'defaultFolder';
+// //   locales = [];
+// //   selectedLocale = '';
+// //   isOriginalSelected = true;
+// //   highlightedText = '';
+// //   showTxtToJson = false;
+
+// //   constructor() {
+// //     makeAutoObservable(this, {}, { autoBind: true });
+// //     this.loadInitialData();
+// //   }
+
+// //   async loadInitialData() {
+// //     const data = await getSettings(this.selectedFolder);
+// //     if (data) {
+// //       this.setAllData(data);
+// //     }
+// //   }
+
+// //   setAllData(data) {
+// //     runInAction(() => {
+// //       this.originalHtml = data.html || '';
+// //       this.foldersData = data.foldersData || {};
+// //       this.updateLocales();
+// //       this.selectedFolder = data.selectedFolder || this.selectedFolder;
+// //       this.selectedLocale = data.selectedLocale;
+// //       this.isOriginalSelected = data.isOriginalSelected;
+// //       this.highlightedText = data.highlightedText;
+// //     });
+// //   }
+
+// //   async saveAllData() {
+// //     if (!this.selectedFolder) {
+// //       console.error('Selected folder is undefined or invalid');
+// //       return;
+// //     }
+// //     await saveSettings({
+// //       html: this.html,
+// //       foldersData: this.foldersData,
+// //       selectedLocale: this.selectedLocale,
+// //       isOriginalSelected: this.isOriginalSelected,
+// //       highlightedText: this.highlightedText
+// //     }, this.selectedFolder);
+// //   }
+
+// //   async deleteFolder(folderName) {
+// //     if (this.foldersData[folderName]) {
+// //       delete this.foldersData[folderName];
+// //       await deleteSettings(folderName);
+// //       this.updateLocales();
+// //       this.saveAllData();
+// //     }
+// //   }
+
+// //   setOriginalHtml(html) {
+// //     runInAction(() => {
+// //       this.originalHtml = html;
+// //       this.html = html;
+// //       this.updateHtmlForAllLocales();
+// //       this.saveAllData();
+// //     });
+// //   }
+
+// //   async handleFilesUploaded(newFoldersData) {
+// //     runInAction(() => {
+// //       this.foldersData = { ...this.foldersData, ...newFoldersData };
+// //       this.updateLocales();
+// //       this.isOriginalSelected = true;
+// //       this.updateHtmlForAllLocales();
+// //       this.saveAllData();
+// //     });
+// //   }
+
+// //   updateLocales() {
+// //     const allLocales = new Set();
+// //     Object.values(this.foldersData).forEach(folder => {
+// //       Object.keys(folder).forEach(locale => allLocales.add(locale));
+// //     });
+// //     runInAction(() => {
+// //       this.locales = Array.from(allLocales);
+// //     });
+// //   }
+// //   async copyToClipboardWithIndication(text, setButtonColor) {
+// //     try {
+// //       await navigator.clipboard.writeText(text);
+// //       setButtonColor('#62d59075');
+// //       setTimeout(() => setButtonColor(''), 700);
+// //     } catch (err) {
+// //       setButtonColor('#f826268c');
+// //       setTimeout(() => setButtonColor(''), 700);
+// //       console.error('Failed to copy text: ', err);
+// //     }
+// //   }
+
+// //   handleFolderSelection(folderName, setButtonColor) {
+// //     if (!folderName) {
+// //       console.error('Attempted to select an undefined or invalid folder');
+// //       return;
+// //     }
+// //     runInAction(() => {
+// //       const text = `$\{{ ${folderName}.block_00 }}$`;
+// //       this.copyToClipboardWithIndication(text, setButtonColor);
+// //     });
+// //   }
+// //   // handleFolderSelection(folderName) {
+// //   //   if (!folderName) {
+// //   //     console.error('Attempted to select an undefined or invalid folder');
+// //   //     return;
+// //   //   }
+// //   //   runInAction(() => {
+// //   //     this.copyToClipboard(`$\{{ ${folderName}.block_00 }}$`);
+// //   //   });
+// //   // }
+
+// //   handleLocaleSelection(locale) {
+// //     runInAction(() => {
+// //       this.selectedLocale = locale;
+// //       this.isOriginalSelected = false;
+// //       this.updateHtmlForLocale(locale);
+// //     });
+// //   }
+
+// //   updateHtmlForLocale(locale) {
+// //     let updatedHtml = this.originalHtml;
+// //     Object.keys(this.foldersData).forEach(folderName => {
+// //       if (this.foldersData[folderName][locale]) {
+// //         updatedHtml = replacePlaceholders(updatedHtml, this.foldersData[folderName][locale], folderName);
+// //       }
+// //     });
+// //     runInAction(() => {
+// //       this.htmlByLocale[locale] = updatedHtml;
+// //       if (locale === this.selectedLocale) {
+// //         this.html = updatedHtml;
+// //       }
+// //     });
+// //   }
+
+// //   updateHtmlForAllLocales() {
+// //     Object.keys(this.htmlByLocale).forEach(locale => {
+// //       this.updateHtmlForLocale(locale);
+// //     });
+// //   }
+
+// //   resetLocale() {
+// //     runInAction(() => {
+// //       this.isOriginalSelected = true;
+// //       this.selectedLocale = '';
+// //       this.html = this.originalHtml;
+// //       this.saveAllData();
+// //     });
+// //   }
+
+// //   setHighlightedText(text) {
+// //     runInAction(() => {
+// //       this.highlightedText = text;
+// //     });
+// //   }
+
+// //   toggleTxtToJson() {
+// //     runInAction(() => {
+// //       this.showTxtToJson = !this.showTxtToJson;
+// //     });
+// //   }
+
+// //   copyToClipboard(text) {
+// //     navigator.clipboard.writeText(text).then(() => {
+// //       this.saveAllData();
+// //     }).catch(err => {
+// //       console.error('Failed to copy text: ', err);
+// //     });
+// //   }
+// // }
+
+// // export const appStore = new AppStore();
+
+// import { makeAutoObservable, runInAction } from "mobx";
+// import { replacePlaceholders } from '../utils/replacePlaceholders';
+// import { saveSettings, getSettings, deleteSettings } from '../utils/indexedDB'; 
+
+// class AppStore {
+//   originalHtml = '';
+//   html = '';
+//   htmlByLocale = {};
+//   foldersData = {};
+//   selectedFolder = 'defaultFolder';
+//   locales = [];
+//   selectedLocale = '';
+//   isOriginalSelected = true;
+//   highlightedText = '';
+//   showTxtToJson = false;
+
+//   constructor() {
+//     makeAutoObservable(this, {}, { autoBind: true });
+//     this.loadInitialData();
+//   }
+
+//   async loadInitialData() {
+//     const data = await getSettings(this.selectedFolder);
+//     if (data) {
+//       this.setAllData(data);
+//     }
+//   }
+
+//   setAllData(data) {
+//     runInAction(() => {
+//       this.originalHtml = data.html || '';
+//       this.foldersData = data.foldersData || {};
+//       this.updateLocales();
+//       this.selectedFolder = data.selectedFolder || this.selectedFolder;
+//       this.selectedLocale = data.selectedLocale;
+//       this.isOriginalSelected = data.isOriginalSelected;
+//       this.highlightedText = data.highlightedText;
+//     });
+//   }
+
+//   async saveAllData() {
+//     if (!this.selectedFolder) {
+//       console.error('Selected folder is undefined or invalid');
+//       return;
+//     }
+//     await saveSettings({
+//       html: this.html,
+//       foldersData: this.foldersData,
+//       selectedLocale: this.selectedLocale,
+//       isOriginalSelected: this.isOriginalSelected,
+//       highlightedText: this.highlightedText
+//     }, this.selectedFolder);
+//   }
+
+//   async deleteFolder(folderName) {
+//     if (this.foldersData[folderName]) {
+//       delete this.foldersData[folderName];
+//       await deleteSettings(folderName);
+//       this.updateLocales();
+//       this.saveAllData();
+//     }
+//   }
+
+//   setOriginalHtml(html) {
+//     runInAction(() => {
+//       this.originalHtml = html;
+//       this.html = html;
+//       this.updateHtmlForAllLocales();
+//       this.saveAllData();
+//     });
+//   }
+
+//   async handleFilesUploaded(newFoldersData) {
+//     runInAction(() => {
+//       this.foldersData = { ...this.foldersData, ...newFoldersData };
+//       this.updateLocales();
+//       this.isOriginalSelected = true;
+//       this.updateHtmlForAllLocales();
+//       this.saveAllData();
+//     });
+//   }
+
+//   updateLocales() {
+//     const allLocales = new Set();
+//     Object.values(this.foldersData).forEach(folder => {
+//       Object.keys(folder).forEach(locale => allLocales.add(locale));
+//     });
+    
+//     // Преобразуем в массив и сортируем
+//     let sortedLocales = Array.from(allLocales).sort();
+    
+//     // Убираем en и ru из отсортированного массива, если они есть
+//     const enIndex = sortedLocales.indexOf('en');
+//     if (enIndex > -1) {
+//       sortedLocales.splice(enIndex, 1);
+//     }
+//     const ruIndex = sortedLocales.indexOf('ru');
+//     if (ruIndex > -1) {
+//       sortedLocales.splice(ruIndex, 1);
+//     }
+    
+//     // Добавляем en и ru в начало, если они есть
+//     if (ruIndex > -1) {
+//       sortedLocales.unshift('ru');
+//     }
+//     if (enIndex > -1) {
+//       sortedLocales.unshift('en');
+//     }
+
+//     runInAction(() => {
+//       this.locales = sortedLocales;
+//     });
+//   }
+
+//   async copyToClipboardWithIndication(text, setButtonColor) {
+//     try {
+//       await navigator.clipboard.writeText(text);
+//       setButtonColor('#62d59075');
+//       setTimeout(() => setButtonColor(''), 700);
+//     } catch (err) {
+//       setButtonColor('#f826268c');
+//       setTimeout(() => setButtonColor(''), 700);
+//       console.error('Failed to copy text: ', err);
+//     }
+//   }
+
+//   handleFolderSelection(folderName, setButtonColor) {
+//     if (!folderName) {
+//       console.error('Attempted to select an undefined or invalid folder');
+//       return;
+//     }
+//     runInAction(() => {
+//       const text = `$\{{ ${folderName}.block_00 }}$`;
+//       this.copyToClipboardWithIndication(text, setButtonColor);
+//     });
+//   }
+
+//   handleLocaleSelection(locale) {
+//     runInAction(() => {
+//       this.selectedLocale = locale;
+//       this.isOriginalSelected = false;
+//       this.updateHtmlForLocale(locale);
+//     });
+//   }
+
+//   updateHtmlForLocale(locale) {
+//     let updatedHtml = this.originalHtml;
+//     Object.keys(this.foldersData).forEach(folderName => {
+//       if (this.foldersData[folderName][locale]) {
+//         updatedHtml = replacePlaceholders(updatedHtml, this.foldersData[folderName][locale], folderName);
+//       }
+//     });
+//     runInAction(() => {
+//       this.htmlByLocale[locale] = updatedHtml;
+//       if (locale === this.selectedLocale) {
+//         this.html = updatedHtml;
+//       }
+//     });
+//   }
+
+//   updateHtmlForAllLocales() {
+//     Object.keys(this.htmlByLocale).forEach(locale => {
+//       this.updateHtmlForLocale(locale);
+//     });
+//   }
+
+//   resetLocale() {
+//     runInAction(() => {
+//       this.isOriginalSelected = true;
+//       this.selectedLocale = '';
+//       this.html = this.originalHtml;
+//       this.saveAllData();
+//     });
+//   }
+
+//   setHighlightedText(text) {
+//     runInAction(() => {
+//       this.highlightedText = text;
+//     });
+//   }
+
+//   toggleTxtToJson() {
+//     runInAction(() => {
+//       this.showTxtToJson = !this.showTxtToJson;
+//     });
+//   }
+
+//   copyToClipboard(text) {
+//     navigator.clipboard.writeText(text).then(() => {
+//       this.saveAllData();
+//     }).catch(err => {
+//       console.error('Failed to copy text: ', err);
+//     });
+//   }
+
+//   addLocaleManually({ locale, keyName, content }) {
+//     const blocks = content.match(/\{\{([\s\S]*?)\}\}/g) || [];
+//     const jsonContent = blocks.reduce((acc, block, index) => {
+//       const key = `block_${String(index).padStart(2, '0')}`;
+//       let value = block.replace(/\{\{|\}\}/g, '').trim();
+//       value = value.replace(/@@(.*?)@@/g, '<b>$1</b>'); // Сохраняем преобразование @@ в <b>
+//       acc[key] = value || " ";
+//       return acc;
+//     }, {});
+  
+//     runInAction(() => {
+//       if (!this.foldersData[keyName]) {
+//         this.foldersData[keyName] = {};
+//       }
+//       this.foldersData[keyName][locale] = jsonContent;
+//       this.updateLocales();
+//       this.updateHtmlForAllLocales();
+//       this.saveAllData();
+//     });
+//   }
+
+//   updateLocale({ locale, keyName, content }) {
+//     const blocks = content.match(/\{\{([\s\S]*?)\}\}/g) || [];
+//     const jsonContent = blocks.reduce((acc, block, index) => {
+//       const key = `block_${String(index).padStart(2, '0')}`;
+//       let value = block.replace(/\{\{|\}\}/g, '').trim();
+//       value = value.replace(/@@(.*?)@@/g, '<b>$1</b>'); // Сохраняем преобразование @@ в <b>
+//       acc[key] = value || " ";
+//       return acc;
+//     }, {});
+
+//     runInAction(() => {
+//       if (!this.foldersData[keyName]) {
+//         this.foldersData[keyName] = {};
+//       }
+//       this.foldersData[keyName][locale] = jsonContent;
+//       this.updateLocales();
+//       this.updateHtmlForAllLocales();
+//       this.saveAllData();
+//     });
+//   }
+
+//   getLocaleData(locale) {
+//     const data = { keyName: '', content: '' };
+//     for (const key in this.foldersData) {
+//       if (this.foldersData[key][locale]) {
+//         data.keyName = key;
+//         data.content = Object.values(this.foldersData[key][locale]).join('\n');
+//         break;
+//       }
+//     }
+//     return data;
+//   }
+// }
+
+// export const appStore = new AppStore();
+
+
 // import { makeAutoObservable, runInAction } from "mobx";
 // import { replacePlaceholders } from '../utils/replacePlaceholders';
 // import { saveSettings, getSettings, deleteSettings } from '../utils/indexedDB'; 
@@ -181,6 +618,7 @@
 // }
 
 // export const appStore = new AppStore();
+
 import { makeAutoObservable, runInAction } from "mobx";
 import { replacePlaceholders } from '../utils/replacePlaceholders';
 import { saveSettings, getSettings, deleteSettings } from '../utils/indexedDB'; 
@@ -196,6 +634,35 @@ class AppStore {
   isOriginalSelected = true;
   highlightedText = '';
   showTxtToJson = false;
+ //-----------------
+  editLocaleModalVisible = false;
+  localeToEdit = null;
+
+  openEditLocaleModal(locale) {
+    this.localeToEdit = locale;
+    this.editLocaleModalVisible = true;
+  }
+
+  closeEditLocaleModal() {
+    this.localeToEdit = null;
+    this.editLocaleModalVisible = false;
+  }
+
+  saveEditedLocale({ locale, keyName, content }) {
+    runInAction(() => {
+      if (!this.foldersData[keyName]) {
+        this.foldersData[keyName] = {};
+      }
+      this.foldersData[keyName][locale] = content;
+
+      this.updateLocales();
+      this.updateHtmlForAllLocales();
+      this.editLocaleModalVisible = false;
+    });
+  }
+
+
+  //-----------------
 
   constructor() {
     makeAutoObservable(this, {}, { autoBind: true });
@@ -418,17 +885,39 @@ class AppStore {
     });
   }
 
+  // getLocaleData(locale) {
+  //   const data = { keyName: '', content: '' };
+  //   for (const key in this.foldersData) {
+  //     if (this.foldersData[key][locale]) {
+  //       data.keyName = key;
+  //       data.content = Object.values(this.foldersData[key][locale]).join('\n');
+  //       break;
+  //     }
+  //   }
+  //   return data;
+  // }
+
+  // getLocaleData(locale) {
+  //   const data = { keyName: '', content: {} };
+  //   for (const key in this.foldersData) {
+  //     if (this.foldersData[key][locale]) {
+  //       data.keyName = key;
+  //       data.content = this.foldersData[key][locale];
+  //       break;
+  //     }
+  //   }
+  //   return data;
+  // }
   getLocaleData(locale) {
-    const data = { keyName: '', content: '' };
+    const data = { placeholders: {} };
     for (const key in this.foldersData) {
       if (this.foldersData[key][locale]) {
-        data.keyName = key;
-        data.content = Object.values(this.foldersData[key][locale]).join('\n');
-        break;
+        data.placeholders[key] = this.foldersData[key][locale];
       }
     }
     return data;
   }
+  
 }
 
 export const appStore = new AppStore();
